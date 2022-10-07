1bebf756c1287ad3daeec703c83c1becd8a05c80
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2022-10-07 12:04:28 -0400

    Set up complete for Full-Stack App

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        App
+      </main>
+    )
+  }
+}
