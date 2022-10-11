c6da0ab93fa448fe283ce597e68d7f836d56c8cb
VivekRana <vivekranarajput@gmail.com>
2022-10-11 14:07:32 -0400

    uncomment some code

--- controllers/property.js
+++ controllers/property.js
@@ -5,10 +5,13 @@ module.exports = {
 }
 
 function create(req, res) {
-    console.log('bodyyyyyyyyyyyyyyyyyy', req.body)
-        //  User.findById(req.user._id, function(err, user){
-        //     user.listProperty.push(req.body)
-        //     user.save()
-        //     res.status(200).json('ok')
-        // })
+    try{
+        User.findById(req.user._id, function(err, user){
+           user.listProperty.push(req.body)
+           user.save()
+           res.status(200).json('ok')
+       })
+    } catch(err){
+        res.status(400).json(err)
+    }
 }
