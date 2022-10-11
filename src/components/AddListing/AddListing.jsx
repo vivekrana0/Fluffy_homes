b4a190adea34af842a1c49d133634eaf5a840a92
VivekRana <vivekranarajput@gmail.com>
2022-10-11 16:19:32 -0400

    include date field in form

--- src/components/AddListing/AddListing.jsx
+++ src/components/AddListing/AddListing.jsx
@@ -7,6 +7,7 @@ export default function AddListingComponent() {
 
   const [propertyData, setPropertyData] = useState({
     address : '',
+    moveInDate: '',
     bedrooms: 0,
     bathrooms: 0,
     parking: 0,
@@ -29,6 +30,7 @@ export default function AddListingComponent() {
         headers: { "Content-Type": "application/json", 'Authorization': 'Bearer' + jwt },
         body: JSON.stringify({
             address : propertyData.address,
+            moveInDate: propertyData.moveInDate,
             bedrooms: propertyData.bedrooms,
             bathrooms: propertyData.bathrooms,
             parking: propertyData.parking,
@@ -57,7 +59,7 @@ export default function AddListingComponent() {
 
       <Form.Group className="mb-3">
         <Form.Label column="lg" lg={2}>Move-In-Date</Form.Label>
-        <Form.Control size="lg" type="date"></Form.Control>
+        <Form.Control size="lg" name='moveInDate' value={propertyData.moveInDate} type="date" onChange={handleChange} ></Form.Control>
       </Form.Group>
 
       <Form.Group className="mb-3">
