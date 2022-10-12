0c4ce0eac1d41abfeb5126749546ac8c4e1bd1b9
VivekRana <vivekranarajput@gmail.com>
2022-10-12 11:22:19 -0400

    aws image upload feature

--- src/components/services/Address.js
+++ src/components/services/Address.js
@@ -4,17 +4,17 @@ import { useState } from "react";
 // console.log(access_token);
 
 const useInput = (incomingValue) => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(incomingValue);
   const [suggestions, setSuggestions] = useState([]);
   
   const handleChange = async (event) => {
+    console.log(event.target.value)
     setValue(event.target.value);
 
     try {
       const endpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${event.target.value}.json?access_token=pk.eyJ1Ijoidml2ZWtwYXRlbDE5OTkiLCJhIjoiY2w5NGdvdDNjMjJ3ajN1bnp0d3YxZGI3eCJ9.X6eU-tp4fob6AiVVH7x9uA&autocomplete=true`;
       const response = await fetch(endpoint);
       const result = await response.json();
-    //   console.log(result);
       setSuggestions(result?.features);
     } catch (error) {
       console.log("Error Fetching Data ", error);
