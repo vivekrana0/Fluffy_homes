afbc97a9faac366ec0b3784fc9a656ea738e3754
Wasoak <Wasimokadia@gmail.com>
2022-10-12 03:30:12 -0400

    update detail page

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -4,12 +4,12 @@ import './Detail.css'
 
 export default function ListingDetailComponent() {
   return (
-    <div>
+    <div className="DetailCard">
     <div style={{  width: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
       <h1>Detail Page</h1>
     </div>
 
-    <div className="DetailCard" style={{  width: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+    <div className="DetailImage" style={{  width: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
       <Image src="https://media.istockphoto.com/photos/for-rent-sign-in-front-of-new-house-picture-id149060607?k=20&m=149060607&s=612x612&w=0&h=To8zlQbIQ66ep7g-C-19aAFi2CdK98sFnLdSWL8zrl4="></Image>
       </div>
     <div className="DetailInfo" >
